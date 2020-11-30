47aa3d855506e3625d11163efb797f2e8dd9d672
Airat Galiev <airat.galiev@gmail.com>
2020-11-30 23:31:46 +0300

    implemented user POST and DELETE methods with using JPA

--- src/main/java/com/github/airatgaliev/restfulwebservices/controller/UserJpaController.java
+++ src/main/java/com/github/airatgaliev/restfulwebservices/controller/UserJpaController.java
@@ -6,23 +6,26 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 import com.github.airatgaliev.restfulwebservices.exception.UserNotFoundException;
 import com.github.airatgaliev.restfulwebservices.model.User;
 import com.github.airatgaliev.restfulwebservices.repository.UserRepository;
-import com.github.airatgaliev.restfulwebservices.service.UserService;
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/jpa")
 public class UserJpaController {
-
-  @Autowired
-  private UserService userService;
 
   @Autowired
   private UserRepository userRepository;
@@ -42,5 +45,23 @@ public class UserJpaController {
     WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
     userEntityModel.add(link.withRel("all-users"));
     return userEntityModel;
+  }
+
+  @PostMapping("/users")
+  public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
+    User savedUser = userRepository.save(user);
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+        .buildAndExpand(savedUser.getId())
+        .toUri();
+    return ResponseEntity.created(uri).build();
+  }
+
+  @DeleteMapping("/users/{id}")
+  public void deleteUser(@PathVariable Integer id) {
+    Optional<User> user = userRepository.findById(id);
+    user.ifPresent(value -> userRepository.delete(value));
+    if (!user.isPresent()) {
+      throw new UserNotFoundException("Don't found user by id: " + id);
+    }
   }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into user values (1, sysdate(), 'Jim');
-insert into user values (2, sysdate(), 'Jack');
-insert into user values (3, sysdate(), 'Jill');
+insert into user values (1001, sysdate(), 'Jim');
+insert into user values (1002, sysdate(), 'Jack');
+insert into user values (1003, sysdate(), 'Jill');
