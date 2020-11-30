73b7d2a2402ce508f9c3b9eb6b19741158f1d1f0
Airat Galiev <airat.galiev@gmail.com>
2020-11-30 23:55:24 +0300

    added Post entity and relationships with Users entity

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
 insert into user values (1001, sysdate(), 'Jim');
 insert into user values (1002, sysdate(), 'Jack');
 insert into user values (1003, sysdate(), 'Jill');
+insert into post values (10001, 'My first post', 1001);
+insert into post values (10002, 'My second post', 1001);
